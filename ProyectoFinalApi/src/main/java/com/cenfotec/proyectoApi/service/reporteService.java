addbf4c85ecfd0df8fc2d3a3ace99fac6d31f166
Arturo Aymerich Panayotte <aaymerichp@ucenfotec.ac.cr>
2020-05-02 11:46:56 -0600

    Api , Paso de Repo
    Api , Paso de Repo

---
+++ ProyectoFinalApi/src/main/java/com/cenfotec/proyectoApi/service/reporteService.java
@@ -0,0 +1,20 @@
+package com.cenfotec.proyectoApi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cenfotec.proyectoApi.domain.Caso;
+
+
+public interface reporteService {
+
+	public Caso saveCaso(Caso newCaso);
+	public List<Caso> getAllCasos();
+	public Optional<Caso> getById(String id);
+	public void deleteById(String id);
+	public List<Caso> reporteCriticos();
+	public List<Caso> reporteCerrados();
+	public List<Caso> reporteMujeres();
+	
+	
+}
